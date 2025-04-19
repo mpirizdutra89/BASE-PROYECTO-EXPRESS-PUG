dabac79646b4d770f25108fbadf77725e759f59d
mpirizdutra89 <mpirizdutra@ulp.edu.ar>
2025-04-19 13:41:00 -0300

    consideraciones

--- template/controllers/inicioController.js
+++ template/controllers/inicioController.js
@@ -1,4 +1,5 @@
 //Aca la idea es manejar routas de la raiz ejemplo http://localhost:3000/   o http://localhost:3000/inicio
+//Rutas etatica que no hagan uso de post,put,delet , es decir solo get
 
 exports.inicio = (req, res) => {
     res.render('inicio');
